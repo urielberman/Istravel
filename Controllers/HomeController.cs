91e90f62d146a3fde67e3229c3e9329cdfe97e0f
URIEL BERMAN KRESERMAN <48521729@ort>
2024-08-07 08:38:50 -0300

    Commit 10

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,8 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         int[] varAux = new int[4];
+        varAux = Models.Sitios.calcularCantSitios(4);
+        varAux[3]--;
         ViewBag.ListaSitios=varAux;
         return View("Index");
     }
